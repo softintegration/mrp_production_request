bae5264c26dd901180e4d0ad9179401682adf03e
Mossaab BOUDCHICHA <mossaab.boudchicha@gmail.com>
2022-12-19 20:02:53 +0100

    Production Request:Put the fields <Requested by,Bill of Material,Source> in readonly mode in the 'waiting' state

--- models/mrp_production_request.py
+++ models/mrp_production_request.py
@@ -3,7 +3,7 @@
 import datetime
 
 from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError,UserError
+from odoo.exceptions import ValidationError, UserError
 from odoo.tools import float_compare, float_round, float_is_zero
 from odoo.tools import format_datetime
 
@@ -45,14 +45,12 @@ class MrpProductionRequest(models.Model):
     date_desired = fields.Datetime('Desired Date', copy=False, index=True, required=True,
                                    states={'draft': [('readonly', False)]}, readonly=True)
     quantity = fields.Float(string="Requested Quantity", digits='Product Unit of Measure', copy=False,
-                            states={'draft': [('readonly', False)]}, readonly=True,requied=True)
-    request_user_id = fields.Many2one('res.users',string='Requested by',default=_get_default_request_user_id,
-                                      states={'done': [('readonly', True)], 'cancel': [('readonly', True)]},
-                                      readonly=False,required=True)
-    approving_user_id = fields.Many2one('res.users',string='Approved by',readonly=True)
+                            states={'draft': [('readonly', False)]}, readonly=True, requied=True)
+    request_user_id = fields.Many2one('res.users', string='Requested by', default=_get_default_request_user_id,
+                                      states={'draft': [('readonly', False)]}, readonly=True, required=True)
+    approving_user_id = fields.Many2one('res.users', string='Approved by', readonly=True)
     bom_id = fields.Many2one(
-        'mrp.bom', 'Bill of Material',
-        readonly=False, states={'done': [('readonly', True)], 'cancel': [('readonly', True)]},
+        'mrp.bom', 'Bill of Material', states={'draft': [('readonly', False)]}, readonly=True,
         domain="""[
         '&',
             '|',
@@ -68,10 +66,9 @@ class MrpProductionRequest(models.Model):
         check_company=True,
         help="Bill of Materials that will be used for the created MO's.")
     origin = fields.Char(
-        'Source', copy=False,
-        states={'done': [('readonly', True)], 'cancel': [('readonly', True)]},
-        help="Reference of the document that generated this production order request.",readonly=False)
-    mrp_production_ids = fields.One2many('mrp.production','mrp_production_request_id',readonly=True)
+        'Source', copy=False, states={'draft': [('readonly', False)]}, readonly=True,
+        help="Reference of the document that generated this production order request.")
+    mrp_production_ids = fields.One2many('mrp.production', 'mrp_production_request_id', readonly=True)
     quantity_produced = fields.Float(string='Produced Quantity', compute='_compute_quantity_produced', store=True)
     note = fields.Html(tring='Note')
     state = fields.Selection([
@@ -80,24 +77,26 @@ class MrpProductionRequest(models.Model):
         ('validated', 'Validated'),
         ('cancel', 'Cancelled')], string='State',
         compute='_compute_state', copy=False, index=True, readonly=True,
-        store=True, tracking=True,default='draft',
+        store=True, tracking=True, default='draft',
         help=" * Draft: The MR is not confirmed yet.\n"
              " * Waiting: The MR is confirmed but waiting for approving.\n"
              " * Validated: The MR is confirmed, the production order can be created.\n"
              " * Cancelled: The MR has been cancelled, can't be confirmed anymore.")
-    mrp_production_ids_count = fields.Integer("Count of linked MOs",compute='_compute_mrp_production_ids_count')
+    mrp_production_ids_count = fields.Integer("Count of linked MOs", compute='_compute_mrp_production_ids_count')
     can_create_mrp_production = fields.Boolean(compute='_compute_can_create_mrp_production')
     company_id = fields.Many2one(
         'res.company', 'Company', default=lambda self: self.env.company,
         index=True, required=True)
 
-    @api.onchange('product_id','company_id')
+    @api.onchange('product_id', 'company_id')
     def _onchange_product_id(self):
         """ Finds UoM of changed product. """
         if not self.product_id:
             self.bom_id = False
-        elif not self.bom_id or self.bom_id.product_tmpl_id != self.product_id.product_tmpl_id or (self.bom_id.product_id and self.bom_id.product_id != self.product_id):
-            bom = self.env['mrp.bom']._bom_find(self.product_id,company_id=self.company_id.id, bom_type='normal')[self.product_id]
+        elif not self.bom_id or self.bom_id.product_tmpl_id != self.product_id.product_tmpl_id or (
+                self.bom_id.product_id and self.bom_id.product_id != self.product_id):
+            bom = self.env['mrp.bom']._bom_find(self.product_id, company_id=self.company_id.id, bom_type='normal')[
+                self.product_id]
             if bom:
                 self.bom_id = bom.id
                 self.product_uom_id = self.bom_id.product_uom_id.id
@@ -125,7 +124,7 @@ class MrpProductionRequest(models.Model):
     def action_make_production_order(self):
         new_wizard = self.env['mrp.production.create'].create({
             'quantity': self.quantity,
-            'product_uom_id':self.product_uom_id.id,
+            'product_uom_id': self.product_uom_id.id,
         })
         view_id = self.env.ref('mrp_production_request.view_mrp_production_create').id
         return {
@@ -142,34 +141,34 @@ class MrpProductionRequest(models.Model):
         return self._action_cancel()
 
     def _action_make_waiting(self):
-        self.write({'state':'waiting'})
+        self.write({'state': 'waiting'})
 
     def _action_validate(self):
-        self.write({'state':'validated','approving_user_id':self.env.user.id})
+        self.write({'state': 'validated', 'approving_user_id': self.env.user.id})
 
-    def _action_make_production_order(self,quantity=False,product_uom_id=False):
-        mrp_production_dict_list = self._prepare_mrp_production(quantity=quantity,product_uom_id=product_uom_id)
+    def _action_make_production_order(self, quantity=False, product_uom_id=False):
+        mrp_production_dict_list = self._prepare_mrp_production(quantity=quantity, product_uom_id=product_uom_id)
         mrp_productions = self.env['mrp.production'].create(mrp_production_dict_list)
         return mrp_productions
-    def _prepare_mrp_production(self,quantity=False,product_uom_id=False):
+
+    def _prepare_mrp_production(self, quantity=False, product_uom_id=False):
         mrp_prod_dict_list = []
         for each in self:
             mrp_prod_dict_list.append({
-                'mrp_production_request_id':each.id,
-                'origin':each.name,
-                'product_id':each.product_id.id,
-                'product_qty':quantity or each.quantity,
-                'product_uom_id':product_uom_id and product_uom_id.id or each.product_uom_id.id,
-                'date_planned_start':each.date_desired,
-                'bom_id':each.bom_id and each.bom_id.id or False,
+                'mrp_production_request_id': each.id,
+                'origin': each.name,
+                'product_id': each.product_id.id,
+                'product_qty': quantity or each.quantity,
+                'product_uom_id': product_uom_id and product_uom_id.id or each.product_uom_id.id,
+                'date_planned_start': each.date_desired,
+                'bom_id': each.bom_id and each.bom_id.id or False,
             })
         return mrp_prod_dict_list
 
     def _action_cancel(self):
-        self.write({'state':'cancel'})
+        self.write({'state': 'cancel'})
 
-
-    def _check_state(self,state):
+    def _check_state(self, state):
         if state == 'validated':
             for each in self:
                 if float_is_zero(each.quantity, precision_rounding=each.product_uom_id.rounding):
@@ -183,9 +182,7 @@ class MrpProductionRequest(models.Model):
     @api.depends('mrp_production_ids')
     def _compute_can_create_mrp_production(self):
         for each in self:
-            each.can_create_mrp_production = len(each.mrp_production_ids.filtered(lambda mp:mp.state != 'cancel')) > 0
-
-
+            each.can_create_mrp_production = len(each.mrp_production_ids.filtered(lambda mp: mp.state != 'cancel')) > 0
 
     @api.depends('mrp_production_ids')
     def _compute_quantity_produced(self):
@@ -195,7 +192,9 @@ class MrpProductionRequest(models.Model):
     def _check_date_range(self):
         for each in self:
             if each.date_request and each.date_desired and each.date_request > each.date_desired:
-                raise ValidationError(_('%s : Desired Date (%s) should be greater than Date (%s)', each.name, format_datetime(self.env, each.date_desired), format_datetime(self.env, each.date_request)))
+                raise ValidationError(_('%s : Desired Date (%s) should be greater than Date (%s)', each.name,
+                                        format_datetime(self.env, each.date_desired),
+                                        format_datetime(self.env, each.date_request)))
         return True
 
     def action_view_mrp_productions(self):

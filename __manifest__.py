a95b684760154a8d97c0ff531c194f71251fb7aa
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-06-24 19:00:55 +0100

    Manufacturing order: Add field «Manufacturing request» field

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
  'qweb': [],
  'description': False,
  'images': [],
- 'version': '1.0.1.32',
+ 'version': '1.0.1.33',
  'category': 'Manufacturing/Manufacturing',
  'demo': [],
  'depends': ['mrp'],
@@ -19,6 +19,7 @@
      'security/ir.model.access.csv',
      'data/mrp_production_request_data.xml',
      'views/mrp_production_request_views.xml',
+     'views/mrp_production_views.xml',
      'views/res_config_settings_views.xml',
      'wizard/mrp_production_create_views.xml'
     ],
